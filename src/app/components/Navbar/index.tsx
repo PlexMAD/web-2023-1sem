d2c285109b826240b9af37a25c259b5fcc2663dd
PlexMAD <113678445+PlexMAD@users.noreply.github.com>
2024-01-08 00:10:35 +0300

    Маршрутизация

---
+++ src/app/components/Navbar/index.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { PAGINATION_ROUTE, CREATE_ROUTE, MEDIAS_ROUTE, INFO_ROUTE } from '../../routes/configs';
+import './styles.css';
+
+interface NavbarProps {
+  isAuth: boolean;
+  setIsAuth: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const Navbar: React.FC<NavbarProps> = ({ isAuth, setIsAuth }) => {
+  const handleAuthToggle = () => {
+    setIsAuth((prevAuth) => !prevAuth);
+  };
+
+  return (
+    <nav className="navbar">
+    <Link to={PAGINATION_ROUTE} className="navbar-link">
+      PAGINATION_ROUTE
+    </Link>
+    <Link to={INFO_ROUTE} className="navbar-link">
+      INFO_ROUTE
+    </Link>
+    <Link to={CREATE_ROUTE} className="navbar-link">
+      CREATE_ROUTE
+    </Link>
+    <Link to={MEDIAS_ROUTE} className="navbar-link">
+      MEDIAS_ROUTE
+    </Link>
+    <button className="auth-button" onClick={handleAuthToggle}>
+      {isAuth ? 'Выйти' : 'Войти'}
+    </button>
+  </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/app/pages/Pagination/index.tsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from "react";
+import { Button, Table } from "antd";
+import axios from "axios";
+import type { ColumnsType } from "antd/es/table";
+interface DataType {
+  id: number;
+  nickname: string;
+  post_count: number;
+  date_of_register: string;
+  achievements: string[];
+}
+const limit = 5;
+function Pagination() {
+  const columns: ColumnsType<DataType> = [
+    { 
+      title: "id",
+      dataIndex: "userId",
+      key: "userId" 
+    },
+    {
+      title: "Ник",
+      dataIndex: "username",
+      key: "username",
+    },
+    {
+      title: "Кол-во постов",
+      dataIndex: "postCount",
+      key: "postCount",
+    },
+    {
+      title: "Дата регистрации",
+      dataIndex: "registrationDate",
+      key: "registrationDate",
+    },
+    {
+      title: "Достижения",
+      dataIndex: "achievements",
+      key: "achievements",
+    },
+  ];
+  const [page, setPage] = useState<number>(1);
+  const [dataSource, setDataSource] = useState<DataType[]>();
+  const getUsers = async (limit: number, page: number) => {
+    const offset = (page - 1) * limit;
+    const response = await axios.get(`http://192.168.0.20:3001/users?offset=${offset}&limit=${limit}`)
+    setDataSource(response.data);    
+  }
+  
+  useEffect(() => { getUsers(limit, page); }, [page])
+
+  return (
+    <div>
+    <div>
+      <Table dataSource={dataSource} columns={columns} pagination={false} />
+    </div>
+    <div>
+      <Button onClick={() => setPage(page - 1)} disabled={page === 1}>
+        Назад
+      </Button>
+      <Button onClick={() => setPage(page + 1)} disabled={page === 4}>
+        Вперед
+      </Button>
+    </div>
+    <p>{page}</p>
+  </div>
+  )
+}
+
+export default Pagination;
