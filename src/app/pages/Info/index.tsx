5d9d3eed3c13fc09471a43256ea7423dac8d13e5
PlexMAD <113678445+PlexMAD@users.noreply.github.com>
2024-05-07 00:23:04 +0300

    1 модуль

--- src/app/pages/Info/index.tsx
+++ src/app/pages/Info/index.tsx
@@ -1,88 +1,110 @@
-import React from 'react';
+import React, { useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
-import { useState } from 'react';
-import { Page, Text, View, Document, StyleSheet, PDFViewer, PDFDownloadLink } from '@react-pdf/renderer';
+import {
+  Page,
+  Text,
+  View,
+  Document,
+  StyleSheet,
+  PDFDownloadLink,
+  Image,
+} from "@react-pdf/renderer";
 
-
-// Create styles
 const styles = StyleSheet.create({
   page: {
-    flexDirection: 'row',
-    backgroundColor: '#E4E4E4'
+    flexDirection: "row",
+    backgroundColor: "#E4E4E4",
   },
   section: {
     margin: 10,
     padding: 10,
-    flexGrow: 1
-  }
+    flexGrow: 1,
+  },
 });
 
 interface IMyForm {
   name: string;
-  picture: File;
+  picture: string; 
 }
 
 const Info = () => {
-  const [task, setTask] = useState<IMyForm>()
+  const [task, setTask] = useState<IMyForm>();
 
   const {
     register,
     handleSubmit,
     formState: { errors },
-    reset
+    reset,
   } = useForm<IMyForm>({
     mode: "onBlur",
-  })
+  });
 
-  const saveElement = (data: IMyForm) => {
-    setTask(data)
+  const saveElement = async (data: IMyForm) => {
+    const pictureBase64 = await convertToBase64(data.picture[0]);
+    setTask({ name: data.name, picture: pictureBase64 });
     reset();
-  }
+  };
+
+  const convertToBase64 = (file: any) =>
+    new Promise<string>((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result as string);
+      reader.onerror = (error) => reject(error);
+    });
 
   return (
     <>
       <form onSubmit={handleSubmit(saveElement)}>
         <input
-          {...register('name', {
+          {...register("name", {
             required: "Поле обязательно для заполнения",
             minLength: {
               value: 5,
-              message: "Нужно больше символов"
-            }
-          }
-          )}
+              message: "Нужно больше символов",
+            },
+          })}
+        />
+        <input
+          type="file"
+          accept="image/*"
+          {...register("picture", {
+            required: "Required",
+          })}
         />
 
         <div>{errors.name?.message}</div>
+        <div>{errors.picture?.message}</div>
         <button type="submit">Сохранить</button>
       </form>
 
-      {!!task?.name && 
-        <PDFDownloadLink document={<MyDocument name={task.name} />} fileName="somename.pdf">
-          {({ blob, url, loading, error }) => (loading ? 'Loading document...' : 'Download now!')}
-        </PDFDownloadLink>}
+      {!!task?.name && (
+        <PDFDownloadLink
+          document={<MyDocument name={task.name} picture={task.picture} />}
+          fileName="somename.pdf"
+        >
+          {({ blob, url, loading, error }) =>
+            loading ? "Loading document..." : "Download now!"
+          }
+        </PDFDownloadLink>
+      )}
     </>
-  )
-}
+  );
+};
 
-interface Props {
-  name: string
-}
-
-const MyDocument:React.FC<Props> = ({name}) => {
+const MyDocument: React.FC<IMyForm> = ({ name, picture }) => {
   return (
     <Document>
-    <Page size="A4" style={styles.page}>
-      <View style={styles.section}>
-        <Text>{name}</Text>
-      </View>
-      <View style={styles.section}>
-        <Text>Section #2</Text>
-      </View>
-    </Page>
-  </Document>
-  )
-}
-
+      <Page size="A4" style={styles.page}>
+        <View style={styles.section}>
+          <Text>{name}</Text>
+        </View>
+        <View style={styles.section}>
+          {picture && <Image src={picture} />}
+        </View>
+      </Page>
+    </Document>
+  );
+};
 
 export default Info;
